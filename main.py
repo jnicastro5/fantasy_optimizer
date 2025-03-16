915116b8fcf764d8608258f3477e1ce8b061eea7
jnicastro5 <jlnicastro5@gmail.com>
2025-03-16 18:10:30 -0500

    advanced authentification features

--- main.py
+++ main.py
@@ -300,7 +300,7 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days'],
 )
 
-name, authentication_status, username = authenticator.login(location='main')
+name, authentication_status, username = authenticator.login()
 
 # Handle authentication status
 if authentication_status:
